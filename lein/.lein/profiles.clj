c7f06a8840bdc993a70f6f67119ae35ae72d88ba
Scott McLeod <halcyonblue@gmail.com>
2016-03-14 15:54:50 -0400

    Adding cider-nrepl back in

--- lein/.lein/profiles.clj
+++ lein/.lein/profiles.clj
@@ -1,4 +1,5 @@
 {:user {:plugins [[refactor-nrepl "2.0.0"]
+                  [cider/cider-nrepl "0.11.0"]
                   [lein-environ "1.0.0"]]
         :dependencies [[org.clojure/tools.nrepl "0.2.12"]
                        [acyclic/squiggly-clojure "0.1.4"]
