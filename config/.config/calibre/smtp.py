fd52326ed74fc7c53d4c8e5645bf0a3340f43d59
Scott McLeod <halcyonblue@gmail.com>
2016-11-13 19:05:10 -0500

    Add Calibre

---
+++ config/.config/calibre/smtp.py
@@ -0,0 +1,36 @@
+# Control email delivery
+
+### Begin group: DEFAULT
+ 
+# from 
+from_ = u'halcyonblue@gmx.com'
+ 
+# accounts
+accounts = cPickle.loads('\x80\x02}q\x01X!\x00\x00\x00halcyonblue-paperwhite@kindle.com]q\x02(U\x19AZW, MOBI, TPZ, PRC, AZW1q\x03\x88\x88es.')
+ 
+# subjects
+subjects = cPickle.loads('\x80\x02}q\x01.')
+ 
+# aliases
+aliases = cPickle.loads('\x80\x02}q\x01.')
+ 
+# tags
+tags = cPickle.loads('\x80\x02}q\x01.')
+ 
+# relay host
+relay_host = u'mail.gmx.com'
+ 
+# relay port
+relay_port = 587
+ 
+# relay username
+relay_username = u'halcyonblue@gmx.com'
+ 
+# relay password
+relay_password = '3f36397d442934373f344033236b'
+ 
+# encryption
+encryption = 'TLS'
+ 
+
+
