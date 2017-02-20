12a9726489223492c82c9f0e3be8ad09e571f2be
Scott McLeod <halcyonblue@gmail.com>
2017-02-19 23:24:33 -0500

    Removing outdated packages

--- emacs/.emacs.d/elpa/quelpa-use-package-20150805.1028/quelpa-use-package-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; quelpa-use-package-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil nil ("quelpa-use-package.el") (22568 4140 773333
-;;;;;;  335000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; quelpa-use-package-autoloads.el ends here
