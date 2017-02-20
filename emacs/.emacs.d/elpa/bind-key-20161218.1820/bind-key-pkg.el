4c74c9a0a0b393079926c9e6cbb43b5b42a4b0a2
Scott McLeod <halcyonblue@gmail.com>
2017-02-19 22:37:34 -0500

    Updating emacs packages

---
+++ emacs/.emacs.d/elpa/bind-key-20161218.1820/bind-key-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "bind-key" "20161218.1820" "A simple way to manage personal keybindings" 'nil :authors '(("John Wiegley" . "jwiegley@gmail.com")) :maintainer '("John Wiegley" . "jwiegley@gmail.com") :url "https://github.com/jwiegley/use-package")
