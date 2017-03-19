824efc46281b6c49b28027499ca0bfddb9fe91cd
Scott McLeod <halcyonblue@gmail.com>
2017-03-18 23:13:04 -0400

    Upgrading emacs packages

--- emacs/.emacs.d/elpa/which-key-2.0.1/which-key-autoloads.el
+++ emacs/.emacs.d/elpa/which-key-2.0.1/which-key-autoloads.el
@@ -3,8 +3,8 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "which-key" "which-key.el" (22698 21722 434967
-;;;;;;  227000))
+;;;### (autoloads nil "which-key" "which-key.el" (22729 52673 870995
+;;;;;;  545000))
 ;;; Generated autoloads from which-key.el
 
 (defvar which-key-mode nil "\
