e365c6d611a5072301074a4474764d5804512b6d
Scott McLeod <halcyonblue@gmail.com>
2017-04-30 18:46:38 -0400

    Move platform level logic to status script

--- tmux/status.sh
+++ tmux/status.sh
@@ -7,13 +7,18 @@ else
     VPN=""
 fi
 
-SYSTEM_BATTERY=$(acpi | awk -F, '{print $2}' | awk '{$1=$1;print}')
+LOCAL_TIME=$(date +%FT%T%:z)
+ZULU_TIME=$(date -u +%FT%TZ)
+
+case `uname` in
+Darwin) echo "${LOCAL_TIME} | ${ZULU_TIME}"
+        ;;
+Linux) SYSTEM_BATTERY=$(acpi | awk -F, '{print $2}' | awk '{$1=$1;print}')
+       echo "${VPN} | ${SYSTEM_BATTERY} | ${LOCAL_TIME} | ${ZULU_TIME}"
+       ;;
+esac
+
 # KEYBOARD_BATTERY=$(cat /sys/class/power_supply/hid-04:db:56:db:5d:96-battery/capacity ||
 #                        cat /sys/class/power_supply/hid-e8:06:88:54:b1:d4-battery/capacity)
 # TRACKPAD_BATTERY=$(cat /sys/class/power_supply/hid-60:c5:47:81:45:70-battery/capacity ||
 #                        cat /sys/class/power_supply/hid-84:fc:fe:e2:b4:29-battery/capacity)
-
-LOCAL_TIME=$(date +%FT%T%:z)
-ZULU_TIME=$(date -u +%FT%TZ)
-
-echo "${VPN} | ${SYSTEM_BATTERY} | ${LOCAL_TIME} | ${ZULU_TIME}"
