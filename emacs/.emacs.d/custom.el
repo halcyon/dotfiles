b2904edf1c53016a1432b6c77af065e1d79d8ff0
Scott McLeod <halcyonblue@gmail.com>
2017-12-07 18:52:17 -0500

    Update emacs packages

--- emacs/.emacs.d/custom.el
+++ emacs/.emacs.d/custom.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(emms-volume-pulse-sink 12)
  '(magit-submodule-arguments nil)
  '(package-selected-packages
    (quote
