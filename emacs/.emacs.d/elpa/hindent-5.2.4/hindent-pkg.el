8f64aa5a8839e1f78757cadd8dac56c5af475626
Scott McLeod <halcyonblue@gmail.com>
2018-01-10 13:32:19 -0500

    upgrading packages

--- emacs/.emacs.d/elpa/hindent-5.2.4/hindent-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "hindent" "5.2.4" "Indent haskell code using the \"hindent\" program" '((cl-lib "0.5")) :commit "e18ec3f55e288883f8042065190572e91651733d" :url "https://github.com/chrisdone/hindent")
