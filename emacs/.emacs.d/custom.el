739468c6b61ff30603c29ba7a7bf3ca66bc2b480
Scott McLeod <halcyonblue@gmail.com>
2018-03-15 16:14:34 -0400

    Add slack, rcirc, and gui ligature support

--- emacs/.emacs.d/custom.el
+++ emacs/.emacs.d/custom.el
@@ -3,11 +3,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(emms-volume-pulse-sink 12)
- '(magit-submodule-arguments nil)
  '(package-selected-packages
    (quote
-    (rcirc-notify projectile helm-projectile emms notmuch geiser org-plus-contrib golden-ratio-scroll-screen golden-ratio yaml-mode yaml avy pkg-info epl ace-window racer cargo rust-mode xclip emojify ht circe request websocket alert gh logito pcache pos-tip flycheck slime macrostep f company yasnippet sbt-mode scala-mode multiple-cursors google edn peg helm kv deferred s dash-functional dash cider use-package diminish bind-key quelpa package-build js2-mode zenburn-theme window-numbering which-key undo-tree smartparens slime-company slack sicp scratch-ext scratch restclient rainbow-delimiters quelpa-use-package pinentry persistent-scratch pbcopy ox-reveal ox-gfm overseer outshine outorg org2jekyll markdown-mode magit htmlize hindent helm-swoop helm-google helm-clojuredocs helm-cider helm-ag haskell-mode gradle-mode goto-last-change git-gutter gist flycheck-pos-tip flycheck-clojure escreen ensime emmet-mode elisp-slime-nav clojure-mode-extra-font-locking clj-refactor buffer-move browse-kill-ring ag))))
+    (zenburn-theme yasnippet yaml-mode xclip which-key undo-tree smartparens slime-company slack scratch-ext scratch restclient rcirc-notify rainbow-delimiters racer quelpa-use-package persistent-scratch ox-reveal ox-gfm overseer outshine outorg org2jekyll org-plus-contrib notmuch multiple-cursors markdown-mode magit js2-mode htmlize hindent helm-swoop helm-projectile helm-google helm-clojuredocs helm-cider helm-ag haskell-mode gradle-mode goto-last-change golden-ratio-scroll-screen golden-ratio git-gutter gist geiser flycheck-pos-tip escreen emms emmet-mode elisp-slime-nav elfeed clojure-mode-extra-font-locking cargo browse-kill-ring ag ace-window))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
