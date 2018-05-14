c54446a05ae4f2064f587ef832a24d13130f9a18
Scott McLeod <halcyonblue@gmail.com>
2018-05-14 14:57:39 -0400

    Remove elfeed and emms

--- emacs/.emacs.d/elpa/clojure-mode-extra-font-locking-5.6.1/clojure-mode-extra-font-locking-autoloads.el
+++
@@ -1,17 +0,0 @@
-;;; clojure-mode-extra-font-locking-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil nil ("../../../../../.emacs.d/elpa/clojure-mode-extra-font-locking-5.6.1/clojure-mode-extra-font-locking-autoloads.el"
-;;;;;;  "../../../../../.emacs.d/elpa/clojure-mode-extra-font-locking-5.6.1/clojure-mode-extra-font-locking.el")
-;;;;;;  (22790 6982 0 0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; clojure-mode-extra-font-locking-autoloads.el ends here
--- emacs/.emacs.d/elpa/emms-4.4/emms-pkg.el
+++
@@ -1,7 +0,0 @@
-(define-package "emms" "4.4" "The Emacs Multimedia System"
-  '((cl-lib "0.5"))
-  :keywords
-  '("emms" "mp3" "mpeg" "multimedia"))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
